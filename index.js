8a595f4ba4d4193f72c8120ebd1dcd4bbaef25fe
arifurrahman01 <arifurrahman.now@gmail.com>
2022-11-24 02:27:16 +0600

    user info save to database added

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@friitraining.a5d8fvh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+
+async function run() {
+    try {
+        const usersCollection = client.db('furnitureWorld').collection('users');
+
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const quary = { email: email }
+            const user = await usersCollection.findOne(quary);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ assessToken: '' });
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+    } finally {
+
+    }
+}
+run().catch(console.log);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Furniture World server is running')
+});
+
+app.listen(port, () => console.log(`Furniture World is running on port ${port}`))
